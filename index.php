e3159f57c145c22749b35503c4741dbfbbdc4d4f
Vitalik <panins899@gmail.com>
2021-07-26 22:21:19 +0300

    Исправил глупые ошибки: убрал ненужный оператор if; убрал приведение к типу int - изменил на float; добавил вывод знака рубля, если число меньше 1000; Впреть буду внимательннее) Таких глупостей допускать не буду.

--- index.php
+++ index.php
@@ -1,52 +1,52 @@
 <?php
 
-    $is_auth = rand(0, 1);
-    $user_name = 'Виталий'; // укажите здесь ваше имя
-    $categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
-    $advertisement = [
-        [
-            "name" => '2014 Rossignol District Snowboard',
-            "category" => 'Доски и лыжи',
-            "price" => '10999',
-            "img" => 'img/lot-1.jpg'
-        ],
-        [
-            "name" => 'DC Ply  Mens 2016/2017 Snowboard',
-            "category" => 'Доски и лыжи',
-            "price" => '159999',
-            "img" => 'img/lot-2.jpg'
-        ],
-        [
-            "name" => 'Крепления Union Contact Pro 2015 года размер L/XL',
-            "category" => 'Крепления',
-            "price" => '8000',
-            "img" => 'img/lot-3.jpg'
-        ],
-        [
-            "name" => 'Ботинки для сноуборда DC Mutiny Charocal',
-            "category" => 'Ботинки',
-            "price" => '10999',
-            "img" => 'img/lot-4.jpg'
-        ],
-        [
-            "name" => 'Куртка для сноуборда DC Mutiny Charocal',
-            "category" => 'Одежда',
-            "price" => '7500',
-            "img" => 'img/lot-5.jpg'
-        ],
-        ["name" => 'Маска Oakley Canopy', "category" => 'Разное', "price" => '5400', "img" => 'img/lot-6.jpg']
-    ];
+$is_auth = rand(0, 1);
+$user_name = 'Виталий'; // укажите здесь ваше имя
+$categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
+$advertisement = [
+    [
+        "name" => '2014 Rossignol District Snowboard',
+        "category" => 'Доски и лыжи',
+        "price" => '10999',
+        "img" => 'img/lot-1.jpg'
+    ],
+    [
+        "name" => 'DC Ply  Mens 2016/2017 Snowboard',
+        "category" => 'Доски и лыжи',
+        "price" => '159999',
+        "img" => 'img/lot-2.jpg'
+    ],
+    [
+        "name" => 'Крепления Union Contact Pro 2015 года размер L/XL',
+        "category" => 'Крепления',
+        "price" => '8000',
+        "img" => 'img/lot-3.jpg'
+    ],
+    [
+        "name" => 'Ботинки для сноуборда DC Mutiny Charocal',
+        "category" => 'Ботинки',
+        "price" => '10999',
+        "img" => 'img/lot-4.jpg'
+    ],
+    [
+        "name" => 'Куртка для сноуборда DC Mutiny Charocal',
+        "category" => 'Одежда',
+        "price" => '7500',
+        "img" => 'img/lot-5.jpg'
+    ],
+    ["name" => 'Маска Oakley Canopy', "category" => 'Разное', "price" => '5400', "img" => 'img/lot-6.jpg']
+];
 
-    function formatSumm(int $number)
-    {
-        $round_numb = ceil($number);
-        if ($round_numb < 1000) {
-            return $round_numb;
-        } else {
-            $finish_price = number_format($round_numb, 0, '', ' ') . ' ₽';
-            return $finish_price;
-        }
+function formatSumm(float $number)
+{
+    $round_numb = ceil($number);
+    if ( $round_numb < 1000 ) {
+        return $round_numb . ' ₽';
+    } else {
+        $finish_price = number_format($round_numb, 0, '', ' ') . ' ₽';
+        return $finish_price;
     }
+}
 
 ?>
 <!DOCTYPE html>
@@ -74,7 +74,7 @@
             <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
             <nav class="user-menu">
-                <? if ($is_auth): ?>
+                <?php if ( $is_auth ): ?>
                     <div class="user-menu__logged">
                         <p><?= $user_name ?></p>
                         <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
@@ -91,7 +91,6 @@
                     </ul>
                 <? endif ?>
                 <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
-
             </nav>
         </div>
     </header>
@@ -100,10 +99,11 @@
         <section class="promo">
             <h2 class="promo__title">Нужен стафф для катки?</h2>
             <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и
-                горнолыжное снаряжение.</p>
+                горнолыжное снаряжение.
+            </p>
             <ul class="promo__list">
                 <!--заполните этот список из массива категорий-->
-                <? foreach ($categories as $category): ?>
+                <?php foreach ($categories as $category): ?>
                     <li class="promo__item promo__item--boards">
                         <a class="promo__link" href="pages/all-lots.html"><?= $category ?></a>
                     </li>
@@ -116,36 +116,28 @@
             </div>
             <ul class="lots__list">
                 <!--заполните этот список из массива с товарами-->
-                <? foreach ($advertisement as $k => $ad): ?>
+                <?php foreach ($advertisement as $k => $ad): ?>
                     <li class="lots__item lot">
                         <div class="lot__image">
-                            <? if ($ad['img'] ?? '') : ?>
-                                <img src="<?= $ad['img'] ?>" width="350" height="260" alt="">
-                            <? endif ?>
+                            <img src="<?= $ad['img'] ?? '' ?>" width="350" height="260" alt="Картинка лота">
                         </div>
                         <div class="lot__info">
                     <span class="lot__category">
-                        <? if ($ad['category'] ?? '') : ?>
-                            <?= $ad['category'] ?>
-                        <? endif ?>
+                            <?= $ad['category'] ?? '' ?>
                     </span>
                             <h3 class="lot__title">
                                 <a class="text-link" href="pages/lot.html">
-                                    <? if ($ad['name'] ?? '') : ?>
-                                        <?= $ad['name'] ?>
-                                    <? endif ?>
+                                    <?= $ad['name'] ?? '' ?>
                                 </a>
                             </h3>
                             <div class="lot__state">
                                 <div class="lot__rate">
-                            <span class="lot__amount">
-                                <? if ($ad['price'] ?? '') : ?>
-                                    <?= $ad['price'] ?>
-                                <? endif ?>
-                            </span>
+                                    <span class="lot__amount">
+                                        <?= $ad['price'] ?? 'По запросу' ?>
+                                    </span>
                                     <span class="lot__cost">
-                                 <?= formatSumm(1200); ?>
-                            </span>
+                                        <?= formatSumm(8212245.34); ?>
+                                    </span>
                                 </div>
                                 <div class="lot__timer timer">
                                     12:23
@@ -163,7 +155,7 @@
     <nav class="nav">
         <ul class="nav__list container">
             <!--заполните этот список из массива категорий-->
-            <? foreach ($categories as $category): ?>
+            <?php foreach ($categories as $category): ?>
                 <li class="nav__item">
                     <a href="pages/all-lots.html"><?= $category ?></a>
                 </li>
