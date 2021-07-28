6420623a7cdd56da28d78f1e45889366c8855538
Vitalik <panins899@gmail.com>
2021-07-28 23:24:58 +0300

    Перенёс массивы и функцию в отдельные файлы, создал шаблоны layout и main, подключил их в index. Добавил фильтрацию от html, js тегов.

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+function formatSumm(float $number)
+{
+    $round_numb = ceil($number);
+    if ( $round_numb < 1000 ) {
+        return $round_numb . ' ₽';
+    } else {
+        $finish_price = number_format($round_numb, 0, '', ' ') . ' ₽';
+        return $finish_price;
+    }
+}
